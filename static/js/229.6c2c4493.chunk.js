3128c0796f89748a8ca8a8f7d9f6e337f431f09a
haihan-lin <haihan.lin.7@gmail.com>
2025-05-18 17:07:56 -0600

    Updates

---
+++ static/js/229.6c2c4493.chunk.js
@@ -0,0 +1,24 @@
+"use strict";(self.webpackChunkutah_chinese_folk_orchestra_website=self.webpackChunkutah_chinese_folk_orchestra_website||[]).push([[229],{3229:(e,i,r)=>{r.r(i),r.d(i,{default:()=>x});var n=r(4647),t=r(3614),s=r(9281),a=r(1319),d=r(9);const c=(0,d.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,l=(0,d.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,o=(0,d.Ay)(a.A)`
+  flex-direction: column
+`,h=(0,d.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`;(0,d.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`;var m=r(579);const x=(0,s.C)()((e=>{let{t:i,id:r,direction:s}=e;return(0,m.jsx)(c,{children:(0,m.jsx)(t.zW,{direction:s,triggerOnce:!0,children:(0,m.jsx)(o,{justify:"space-between",align:"middle",id:r,children:(0,m.jsx)(n.A,{lg:11,md:11,sm:11,xs:24,children:(0,m.jsxs)(h,{children:[(0,m.jsx)("h6",{children:i("Contact")}),(0,m.jsxs)(l,{children:[i("Please email")," ",(0,m.jsx)("a",{href:"mailto:orchestra@chineseutah.org",children:"orchestra@chineseutah.org"})," ",i("or call (801) 232-0571 for performance or audition inquiries.")]})]})})})})})}))}}]);
+//# sourceMappingURL=229.6c2c4493.chunk.js.map
---
+++ static/js/566.404eecfc.chunk.js
@@ -0,0 +1,45 @@
+"use strict";(self.webpackChunkutah_chinese_folk_orchestra_website=self.webpackChunkutah_chinese_folk_orchestra_website||[]).push([[566],{6566:(e,i,t)=>{t.r(i),t.d(i,{default:()=>w});var n=t(4647),r=t(3614),s=t(9281),d=t(5639),c=t(2646),o=t(1319),a=t(9);const l=(0,a.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,h=(0,a.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,m=(0,a.Ay)(o.A)`
+  flex-direction: ${e=>{let{direction:i}=e;return"left"===i?"row":"row-reverse"}};
+`,x=(0,a.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,p=((0,a.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,(0,a.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,(0,a.Ay)("p")`
+  font-size: 13px;
+`,(0,a.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`);var f=t(579);const w=(0,s.C)()((e=>{let{icon:i,title:t,content:s,section:o,button:a,t:w,id:y,direction:u}=e;return(0,f.jsx)(l,{children:(0,f.jsx)(r.zW,{direction:u,triggerOnce:!0,children:(0,f.jsxs)(m,{justify:"space-between",align:"middle",id:y,direction:u,children:[(0,f.jsx)(n.A,{lg:11,md:11,sm:12,xs:24,children:(0,f.jsx)(c.A,{src:i,width:"100%",height:"100%"})}),(0,f.jsx)(n.A,{lg:11,md:11,sm:11,xs:24,children:(0,f.jsxs)(x,{children:[(0,f.jsx)("h6",{children:w(t)}),(0,f.jsx)(h,{children:w(s)}),(0,f.jsx)(p,{children:"object"===typeof a&&a.map(((e,i)=>(0,f.jsx)(d.$,{color:e.color,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})("contact"),children:w(e.title)},i)))})]})})]})})})}))}}]);
+//# sourceMappingURL=566.404eecfc.chunk.js.map
